18befcc00649ad4c1f93504405e8cfcc7445327a
Hitesh Agarwal <hiteshagarwal135h@gmail.com>
2020-03-26 13:04:20 +0530

    Shifting avg, min and max value varibale globally & value set by threads

--- statistics_multi_thread.c
+++ statistics_multi_thread.c
@@ -1,41 +1,48 @@
 #include<stdio.h>
 #include<pthread.h>
+
 int size=7;
+int avg_val,min_val,max_val;
+
 void *average(void *args);
 void *minimum(void *args);
 void *maximum(void *args);
+
 int main(){
 	pthread_t avg,min,max;
 	int val[7]={90,81,78,95,79,72,85};
 	pthread_create(&avg,NULL,average,(void*)val);
 	pthread_create(&min,NULL,minimum,(void*)val);
 	pthread_create(&max,NULL,maximum,(void*)val);
-	pthread_join(avg,NULL);	
+	pthread_join(avg,NULL);
 	pthread_join(min,NULL);
 	pthread_join(max,NULL);
+	printf("The average value is %d\n",avg_val);
+	printf("The minimun value is %d\n",min_val);
+	printf("The maximum value is %d\n",max_val);
+return 0;
 }
 void *average(void *args){
 	int *array=(int*)args;
-	int sum,i;
+	int sum=0,i;
 	for(i=0;i<size;i++)
-		sum+=array[i];
-	printf("The average value is %d\n",sum/size);
+	     sum=sum+array[i];
+	avg_val = sum/size;
 }
 void *minimum(void *args){
-	int *array=(int*)args;
-	int min=array[0],i;
-	for(i=1;i<size;i++){
-		if(!(min<array[i]))
-			min=array[i];
+	int *array = (int*)args;
+	min_val=array[0];
+	for(int i=1;i<size;i++){
+		if(!(min_val<array[i]))
+			min_val=array[i];
 	}
-	printf("The minimun value is %d\n",min);
+	
 }
 void *maximum(void *args){
-	int *array=(int*)args;
-	int max=array[0],i;
-	for(i=1;i<size;i++){
-		if(max<array[i])
-			max=array[i];
+	int *array = (int*)args;
+	max_val=array[0];
+	for(int i=1;i<size;i++){
+		if(max_val<array[i])
+			max_val=array[i];
 	}
-	printf("The maximum value is %d\n",max);
 }
